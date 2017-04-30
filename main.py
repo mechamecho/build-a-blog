ed6ca181a009becf239025466f179de663cc9034
Livepurposefully <nimiko_chan@live.com>
2017-04-29 20:55:45 -0400

    Create Art entity for database with title, art, and created properties

--- main.py
+++ main.py
@@ -35,6 +35,12 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
     	self.write(self.render_str(template, **kw))
 
+#creating an entity called art for the database
+class Art(db.Model):
+	title=db.StringProperty(required = True)
+	art=db.TextProperty(required = True)
+	created=db.DateTimeProperty(auto_now_add = True)
+
 
 class MainPage(Handler):
 
