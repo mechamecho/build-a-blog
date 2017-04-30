8c318895d0e23d6ad9960226f97538857bffb574
Livepurposefully <nimiko_chan@live.com>
2017-04-29 23:09:56 -0400

    Changed Art entity into Post Entity, and the art property to a post property

--- main.py
+++ main.py
@@ -35,40 +35,40 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
     	self.write(self.render_str(template, **kw))
 
-#creating an entity called art for the database
-class Art(db.Model):
+#creating an entity called post for the database
+class Post(db.Model):
 	title=db.StringProperty(required = True)
-	art=db.TextProperty(required = True)
+	post=db.TextProperty(required = True)
 	created=db.DateTimeProperty(auto_now_add = True)
 
 
 class MainPage(Handler):
 
-	def render_front(self, title="", art="", error=""):
-		arts=db.GqlQuery("SELECT * FROM Art ORDER BY created DESC")
-		self.render("front.html", title=title, art=art, error=error, arts=arts)
+	def render_front(self, title="", post="", error=""):
+		posts=db.GqlQuery("SELECT * FROM post ORDER BY created DESC")
+		self.render("front.html", title=title, post=post, error=error, posts=posts)
 
 	def get(self):
 		self.render_front()
 
 	def post(self):
-		# to get the title and art from the request, to validate
+		# to get the title and post from the request, to validate
 		title=self.request.get("title")
-		art=self.request.get("art")
+		post=self.request.get("post")
 
 		#checking if one of the fields was left empty, to 
 		#render the form again
-		if title and art:
-			#creating a new Art instance with the user input 
+		if title and post:
+			#creating a new post instance with the user input 
 			# and saving it to the data base 
-			a= Art(title= title, art= art)
+			a= post(title= title, post= post)
 			a.put()
 			
 			#redirect to the frontpage to avoid reload message
 			self.redirect("/")
 		else:
-			error="we need both a title and some artwork!"
-			self.render_front(title, art, error)
+			error="we need both a title and some postwork!"
+			self.render_front(title, post, error)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
